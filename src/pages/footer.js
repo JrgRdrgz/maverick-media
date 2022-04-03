df56bacbc0ef32074e891159f762cbbaf2eaa9ec
Jorge Rodriguez <jorerodriguez98@gmail.com>
2022-04-03 15:47:15 -0500

    added social media icons to the footer

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import "../styles/footer.css";
+import facebook from '../assets/facebook.png';
+import twitter from '../assets/twitter.png';
+import instagram from '../assets/instagram.png';
+import tiktok from '../assets/tiktok.png';
+import youtube from '../assets/youtube.png';
 
 function footer() {
     return (
         <div className="footer">
-            <div className="socials"></div>
+            <h3 className="socials"></h3>
+            <img src={facebook} className="icons"></img>
+            <img src={twitter} className="icons"></img>
+            <img src={instagram} className="icons"></img>
+            <img src={tiktok} className="icons"></img>
+            <img src={youtube} className="icons"></img>
         </div>
     )
 }
--- src/pages/media.js
+++ src/pages/media.js
@@ -9,7 +9,6 @@ function media() {
         </div>
         <div id="video-container">
             <iframe width="853" height="505" src="https://www.youtube.com/embed/?listType=playlist&list=PLBV-eXu-9hfA-cRRe_-QV381UMNLLGcD4&autoplay=1" frameBorder="0" allowFullScreen></iframe>
-
         </div>
     </div>
   )
