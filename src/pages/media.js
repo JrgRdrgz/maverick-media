6a2b30c0b94b2cea1e282db299c82af0d7f7c3e5
Jorge Rodriguez <jorerodriguez98@gmail.com>
2022-04-04 21:26:35 -0500

    fixed the bottom padding in the about us page. and cleaned up the media page for iteration 3

--- src/pages/media.js
+++ src/pages/media.js
@@ -8,10 +8,8 @@ function media() {
             <p className="lofi">Mav Media Videos</p>
         </div>
         <div id="video-container">
-            <iframe width="1120" height="630" src="https://www.youtube.com/embed/?listType=playlist&list=PLBV-eXu-9hfA-cRRe_-QV381UMNLLGcD4&autoplay=1" title="YouTube video player" frameBorder="0" allowFullScreen></iframe>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/MbyPajnyuKQ?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/vPVmtiz6sA4?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/uudJaZLnKWg?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="854" height="480" src="https://www.youtube.com/embed/?listType=playlist&list=PLBV-eXu-9hfA-cRRe_-QV381UMNLLGcD4&autoplay=1" title="YouTube video player" frameBorder="0" allowFullScreen></iframe>
+        
         </div>
         
     </div>
@@ -19,3 +17,7 @@ function media() {
 }
 
 export default media
+
+//<iframe width="560" height="315" src="https://www.youtube.com/embed/MbyPajnyuKQ?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+//<iframe width="560" height="315" src="https://www.youtube.com/embed/vPVmtiz6sA4?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+//<iframe width="560" height="315" src="https://www.youtube.com/embed/uudJaZLnKWg?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
